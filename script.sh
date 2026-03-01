25b27f4ed02ca68cb3e7c5509d132dfb075e0a6d
yassin-elkhamlichi <yassinelkhamlichi98@gmail.com>
2026-03-01 17:16:42 +0000

    create project structure

---
+++ script.sh
@@ -0,0 +1,7 @@
+# Create directories
+mkdir assets routes tasks
+
+# Create empty files
+touch .env .env.example .gitignore docker-compose.yml Dockerfile LICENSE main.py  requirements.txt
+
+echo "Project structure created successfully!"
