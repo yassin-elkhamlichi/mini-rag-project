eb2d48cb48875fe0e1e522c1e877052b6eae03fd
yassin-elkhamlichi <yassinelkhamlichi98@gmail.com>
2026-03-02 14:02:17 +0000

    rafactoring the code

--- src/controllers/DataService.py
+++ src/controllers/DataService.py
@@ -1,10 +1,10 @@
 from fastapi import UploadFile, status, HTTPException
-from .BaseService import BaseService
+from .BaseService import BaseController
 from src.models import ResponseMessage
-from .ProjectService import ProjectService
+from .ProjectController import ProjectController
 import re
 
-class DataService(BaseService):
+class DataController(BaseController):
     
     def __init__(self):
         super().__init__()
@@ -30,7 +30,7 @@ class DataService(BaseService):
     def generate_unique_filename(self, orig_file: str, project_id: str):
 
         random_filename = self.generate_random_string()
-        project_path = ProjectService().get_project_path(project_id=project_id)
+        project_path = ProjectController().get_project_path(project_id=project_id)
         cleaned_name_file = self.clean_filename(orig_file=orig_file)
 
         final_filename = f"{random_filename}_{cleaned_name_file}"
